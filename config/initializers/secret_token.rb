a93468b4ee74720ae5405e03d5a7baee3136f3bf
Vignesh Rajagopalan <vignesh@campuspry.com>
2011-08-04 12:29:52 +0530

    Created a Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnivissuesCom::Application.config.secret_token = 'eefbf0e490a56a52281c6122f97cd19dc025fdb7d4ec4a4ce4ccbee0b73717d1285ab61993e398c850f316af6621632adb8e729f106aadca447145f32eb6c539'
