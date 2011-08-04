90dad4be009f2fec1e3df2156b43816bf97ac688
Vignesh Rajagopalan <vignesh@campuspry.com>
2011-08-04 22:12:29 +0530

    Home controller/layouts were added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-UnivissuesCom::Application.routes.draw do
+  UnivissuesCom::Application.routes.draw do
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,6 @@ UnivissuesCom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
